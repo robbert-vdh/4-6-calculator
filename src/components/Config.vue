96c330533cd4e1727740d8b9db88535fa9f81496
Robbert van der Helm <mail@robbertvanderhelm.nl>
2017-09-28 15:56:08 +0200

    Move the configuration to a seperate component

---
+++ src/components/Config.vue
@@ -0,0 +1,93 @@
+<template>
+<div>
+  <div>
+    <h2>Parameters</h2>
+    <div class="grid-x grid-margin-x">
+      <div class="medium-6 cell">
+        <div class="grid-x grid-padding-x">
+          <div class="shrink cell">
+            <label for="coffee-weight" class="text-right middle">Coffee</label>
+          </div>
+          <div class="auto cell">
+            <div class="input-group">
+              <input :value="coffeeWeight" @input="$emit('update:coffeeWeight', Number($event.target.value))"
+                     id="coffee-weight" class="input-group-field" type="number" min="0">
+              <span class="input-group-label">grams</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="medium-6 cell">
+        <div class="grid-x grid-padding-x">
+          <div class="shrink cell">
+            <label for="ratio" class="text-right middle">Ratio</label>
+          </div>
+          <div class="auto cell">
+            <div class="input-group">
+              <span class="input-group-label">1 :</span>
+              <input :value="ratio" @input="$emit('update:ratio', Number($event.target.value))"
+                     id="ratio" class="input-group-field" type="number" min="0" step="0.1">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="medium-6 cell">
+        <div class="grid-x grid-padding-x">
+          <span class="shrink cell input-group-label">Sweet</span>
+          <input :value="sweetnessRatio" @input="$emit('update:sweetnessRatio', Number($event.target.value))"
+                 class="auto cell slider" type="range" min="0" max="1" step="0.02">
+          <span class="shrink cell input-group-label">Acidic</span>
+        </div>
+      </div>
+      <div class="medium-6 cell">
+        <div class="grid-x grid-padding-x">
+          <div class="shrink cell">
+            <label for="ratio" class="text-right middle">Intensity</label>
+          </div>
+          <div class="auto cell">
+            <input :value="intensity" @input="$emit('update:intensity', Number($event.target.value))"
+                   class="auto cell slider" type="range" min="1" max="5" step="1">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class Config extends Vue {
+  /** The total weight of the coffee grounds. */
+  @Prop({type: Number, required: true})
+  coffeeWeight: number;
+
+  /** The 1:x coffee to water ratio, in mass. */
+  @Prop({type: Number, required: true})
+  ratio: number;
+
+  /**
+   * The acidity/sweetness ratio of the first 40% of the pour. 0.42
+   * corrosponds to Tetsu Kasuya's original 50:70 ratio.
+   */
+  @Prop({type: Number, required: true})
+  sweetnessRatio: number;
+
+  /** The number of pours the last 60% will be devided into. */
+  @Prop({type: Number, required: true})
+  intensity: number;
+}
+</script>
+
+<style lang="scss">
+// TODO: Remove scaffolding
+@import '~foundation-sites/scss/foundation';
+
+@include foundation-range-input;
+
+.slider {
+  margin: 0 0.5rem;
+}
+</style>
