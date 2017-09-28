dc30edaa78fd9300b79fea502e4c33c8e77f6129
Robbert van der Helm <mail@robbertvanderhelm.nl>
2017-09-28 16:46:45 +0200

    Move the calculations to a single function

--- src/components/App.vue
+++ src/components/App.vue
@@ -4,9 +4,7 @@
           :intensity.sync="intensity" :acidity.sync="acidity" />
   <p>Total mass: {{ totalMass }} g</p>
   <ul>
-    <li>First pour: {{ firstPart[0] }} g</li>
-    <li>Second pour: {{ firstPart[1] }} g</li>
-    <li v-for="(amount, index) in secondPart">
+    <li v-for="(amount, index) in pours">
       {{ ORDINALS[index] }} pour: {{ amount }} g
     </li>
   </ul>
@@ -28,8 +26,10 @@ const swek = 69;
   components: {Config}
 })
 export default class App extends Vue {
-  /** Used for naming the pours in the second part. */
-  readonly ORDINALS = ['Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh'];
+  /** Used for naming the pours. */
+  readonly ORDINALS = [
+    'First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh'
+  ];
 
   acidity = 5;
   coffeeWeight = 20.0;
@@ -40,27 +40,15 @@ export default class App extends Vue {
     return Math.round(this.coffeeWeight * this.ratio);
   }
 
-  /**
-   * The first two pours controlling acidity and sweetness.
-   */
-  get firstPart(): [number, number] {
-    const ratio = this.acidity / TOTAL_ACIDITY_SWEETNESS;
-    const total = this.totalMass * FIRST_PART_RATIO;
+  get pours(): number[] {
+    const acidityRatio = this.acidity / TOTAL_ACIDITY_SWEETNESS;
+    const intensityRatio = 1.0 / this.intensity;
+    const firstPart = this.totalMass * FIRST_PART_RATIO;
+    const secondPart = this.totalMass * SECOND_PART_RATIO;
 
-    return [Math.round(total * ratio), Math.round(total)];
-  }
-
-  /**
-   * The first two pours controlling acidity and sweetness.
-   */
-  get secondPart(): number[] {
-    const ratio = 1.0 / this.intensity;
-    const total = this.totalMass * SECOND_PART_RATIO;
-
-    let pours = [];
-    const offset = this.totalMass * FIRST_PART_RATIO;
+    let pours = [Math.round(acidityRatio * firstPart), Math.round(firstPart)]
     for (let i = 1; i <= this.intensity; i++) {
-      pours.push(offset + total * ratio * i);
+      pours.push(Math.round(firstPart + secondPart * intensityRatio * i));
     }
 
     return pours;
